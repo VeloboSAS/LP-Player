c031e762d177de08358f6ccdc5ce5a39f7baaa6c
Alex <alexss81@mail.ru>
2025-10-13 13:23:29 +0300

    conditional rendering

--- lp-player/src/App.tsx
+++ lp-player/src/App.tsx
@@ -4,31 +4,56 @@ import Numb from "./sound/Numb.mp3"
 import End from "./sound/End.mp3"
 import Faint from "./sound/Faint.mp3"
 
-function App() {
-  const tracks = [
-    { id:1, title: "LP track1", url: Pale },
-    { id:2, title: "LP track2", url: Rest },
-    { id:3, title: "LP track3", url: Numb },
-    { id:4, title: "LP track4", url: End },
-    { id:5, title: "LP track5", url: Faint },
 
-  ]
+// const tracks = null
+const tracks = [
+  { id: 1, title: "LP track1", url: Pale },
+  { id: 2, title: "LP track2", url: Rest },
+  { id: 3, title: "LP track3", url: Numb, isSelected: true },
+  { id: 4, title: "LP track4", url: End },
+  { id: 5, title: "LP track5", url: Faint },
+  { id: 6, title: "LP track6", url: " https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3" },
+
+]
+
+const selectedTrackId = 6
+
+
+export function App() {
+
+  if (tracks === null) {
+    return <> <div>
+      <h1>LP Player</h1>
+      <span>Loading...</span>
+    </div>
+    </>
+  }
+
+  if (tracks.length === 0) {
+    return <> <div>
+      <h1>LP Player</h1>
+      <span>No tracks</span>
+    </div>
+    </>
+  }
 
   return (
     <>
       <h1>LP Player</h1>
-      <ui>
+      <ul>
         {tracks.map((track) => {
-          return <li key ={track.id}>
+          return <li key={track.id} style={{
+            border: track.id === selectedTrackId ? '1px solid orange' : 'none'
+          }}>
             <div>
               {track.title}
             </div>
             <audio src={track.url} controls></audio>
           </li>
         })}
-      </ui>
+      </ul>
+
+
     </ >
   )
 }
-
-export default App
