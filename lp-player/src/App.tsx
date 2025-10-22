ca9e2906f4baa382b746d5ee7e5761a9230f0755
Alex <alexss81@mail.ru>
2025-10-22 17:33:49 +0300

    Details

--- lp-player/src/App.tsx
+++ lp-player/src/App.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react"
 export function App() {
 
   const [selectedTrackId, setSelectedTrackId] = useState(null)
+  const [selectedTrack, setSelectedTrack] = useState(null)
   const [tracks, setTracks] = useState(null
   )
 
@@ -12,7 +13,7 @@ export function App() {
     console.log("effect")
     fetch("https://musicfun.it-incubator.app/api/1.0/playlists/tracks", {
       headers: {
-        "api-key": "",
+        "api-key": "6055cc46-51d8-48ee-bcf2-77280ab51704",
       },
     })
       .then((res) => res.json())
@@ -38,26 +39,56 @@ export function App() {
     </>
   }
 
+
   return (
     <>
-      <h1 style={{marginLeft: 40}}>LP Player</h1>
-      <button style={{marginLeft: 40, backgroundColor: 'blue'}} onClick={() => {
-        setSelectedTrackId(null)
+      <h1 style={{ marginLeft: 40 }}>LP Player</h1>
+      <button style={{ marginLeft: 40, backgroundColor: 'blue' }} onClick={() => {
+        setSelectedTrackId(null),
+
+        setSelectedTrack(null)
       }}>Reset selection</button>
-      <ul>
-        {tracks.map((track) => {
-          return <li key={track.id} style={{
-            border: track.id === selectedTrackId ? '1px solid orange' : 'none'
-          }}>
-            <div onClick={() => {
-              setSelectedTrackId(track.id)
+      <div style={{ display: 'flex', gap: 30 }}>
+        <ul>
+          {tracks.map((track) => {
+            return <li key={track.id} style={{
+              border: track.id === selectedTrack?.id ? '1px solid orange' : 'none'
             }}>
-              {track.attributes.title}
+              <div onClick={() => {
+                setSelectedTrackId(track.id)
+                fetch("https://musicfun.it-incubator.app/api/1.0/playlists/tracks/" + track.id,
+                  {
+                    headers: {
+                      "api-key": "6055cc46-51d8-48ee-bcf2-77280ab51704",
+                    },
+                  })
+                  .then((res) => res.json())
+                  .then((json) => {
+                    setSelectedTrack(json.data)
+                  })
+                setSelectedTrack(track)
+              }}>
+                {track.attributes.title}
+              </div>
+              <audio src={track.attributes.attachments[0].url} controls></audio>
+            </li>
+          })}
+        </ul>
+        <div>
+          <h2>Details</h2>
+          {selectedTrack === null ? 'Track is not selected' :
+            <div>
+              <h3>{selectedTrack.attributes.title}</h3>
+              <h4>Lyrics</h4>
+              <p>
+                {selectedTrack.attributes.lyrics ?? 'No lyrics'}
+              </p>
             </div>
-            <audio src={track.attributes.attachments[0].url} controls></audio>
-          </li>
-        })}
-      </ul>
+          }
+        </div>
+
+      </div>
+
 
 
     </ >
